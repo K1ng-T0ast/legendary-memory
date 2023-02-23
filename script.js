680b93609c47e1bd6794019783eab936afeb9491
K1ng-T0ast <kylejwhite@proton.me>
2023-02-22 17:07:10 -0800

    Lab 06 execute

---
+++ script.js
@@ -0,0 +1,26 @@
+// let myName = "Kyle";
+// /* alert("Welcome to the THUNDER DOME" + myName); */
+// const myFavColor = "blue";
+
+// console.log(myFavColor)
+
+// console.log ("May the force be with you")
+
+// myFavColor = "purple";
+// console.log(myFavColor);
+
+// let userName = prompt("what is your name?");
+
+// alert("Hello " + userName");
+
+let foodChoice = prompt("Tired of eating fast food? Type yes or no");
+
+if(foodChoice == 'yes'){
+    document.write("Welcome! You're in good oven mitt hands!");
+} else if(foodChoice == 'no'){
+        document.write("No judgement here!...well, perhaps a tablespoon of judgement..")
+     } else {
+        document.write("Is nihilism a cuisine?")
+     };
+
+console.log("foodChoice");
