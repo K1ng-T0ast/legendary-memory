4fc8bfe5262c7d770d507b8703941a39b9ad9454
K1ng-T0ast <kylejwhite@proton.me>
2023-02-24 13:06:57 -0800

    Lab 08 execute

--- script.js
+++ script.js
@@ -17,32 +17,71 @@
 // console.log(FooodChoice);
 
 // .twoLowerCase()
-
+// if (foodChoice == 'yes' || foodChoice == 'YES' || foodChoice == 'Yes') {
 
 function likesFood() {
-    let foodChoice = prompt("Tired of eating fast food? Type yes or no");
+    let foodChoice = prompt("Tired of eating fast food? Type yes or no").toLowerCase();
     console.log(foodChoice);
-
-    // let promptChoice = (yes, no);
-
-    if (foodChoice == 'yes') {
-        alert("Welcome! You're in good oven mitt hands!");
-    } else if (foodChoice == 'no') {
-        let promptChoice = prompt("Are you sure?");
-        if (promptChoice == 'yes') {
-            prompt("Iron Chef Morimoto says: Try Again..")
-        }
-        if (promptChoice == 'yes') {
-            alert("The Iron Chefs are conferring upon your decision, prepare thy apron..")
-        }
-        else if (promptChoice == 'no') {
-            alert("You have chosen wisely, grasshoppa..");
-        }
-    } else {
-        alert("Is nihilism a cuisine?");
-    }
-    return foodChoice;
+    while (foodChoice !== 'yes') {
+        foodChoice = prompt("Gordon Ramsay says guess again..")
+    } 
+        alert("Love the encouragement, you win a prize!");
 }
+// likesFood();
+
+function userReward() {
+    let rewardPics = parseInt(prompt("Your dilligence is rewarded with pics of Gordon Ramsay, how many would you like to see? Pick between 1 & 25!"));
+    console.log("user wants " + rewardPics + "pics");
+
+    while (rewardPics > 25) {
+        rewardPics = parseInt(prompt("Ramsay says to pick a number between 1 & 25"));
+        console.log("user wants " + rewardPics + "pics");
+
+    } 
+        alert("YOU get a Ramsay! (happy Oprah noises)");
+        console.log("user wants " + rewardPics + "pics");
+        for (let i = 0; i < rewardPics; i++) {
+            document.write('<img src="images/ramsay.jpg">');
+        }
+}
+// userReward();
+
+
+
+//  likesFood();       
+
+
+// for (let i = 0; i < 20; i++){
+//     display=img src="images/ramsay.jpg";
+// }
+
+// let sum = 0;
+
+// while (true){
+//     let value = +prompt("Enter a Number", '');
+//     if (!value) break;
+//     sum +=value;
+// }
+// console.log( 'Sum: ' + sum);
+
+
+// if (foodChoice == 'yes') {
+//     alert("Welcome! You're in good oven mitt hands!");
+// } else if (foodChoice == 'no') {
+//     let promptChoice = prompt("Are you sure?");
+//     if (promptChoice == 'yes') {
+//         prompt("Iron Chef Morimoto says: Try Again..")
+//     }
+//     if (promptChoice == 'yes') {
+//         alert("The Iron Chefs are conferring upon your decision, prepare thy apron..")
+//     }
+//     else if (promptChoice == 'no') {
+//         alert("You have chosen wisely, grasshoppa..");
+//     }
+// } else {
+//     alert("Is nihilism a cuisine?");
+// }
+// return foodChoice;
 
 function okay() {
     if (confirm("Should I tell Gordon Ramsey about this?")) {
@@ -51,7 +90,7 @@ function okay() {
         document.write("OK, I won't, but you're missing out on a phone call from a very stern but fair British man.");
     }
 }
-
+console.log(okay);
 function goodbye() {
     document.write("Believe, and one day you will be your own Iron Chef! See you in the kitchen!");
 }
